8230d8444f12ef4a449df26f7832ac5d29c05c71
Alex <alex.weber312@gmail.com>
2018-07-13 19:03:32 +0200

    *established project structure *added vue,vue-router & vue-resources

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Test for SASS'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "sass/main";
+</style>
