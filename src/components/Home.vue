389e78d2a6cf9ed457f4e710246926e3bcee3ccf
Alex <alex.weber312@gmail.com>
2018-07-17 21:36:12 +0200

    *some styling home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,33 @@
 <template>
-<ul>
-  <li v-for="value in allData">
-     <div class="row">
-      <div class="col s12 m6">
-        <div class="card large darken-2">
-          <div class="card-content">
-            <span class="card-title">{{value.name}}</span>
-            <ul>
-              <li>Floors: {{value.floors.length}}</li>
-              <li>Rooms: {{value.rooms.length}}</li>
-              <!-- <li>qm: {{value.floors}}</li> -->
-            </ul>
-          </div>
-          <div class="card-tabs">
-            <ul class="tabs tabs-fixed-width">
-              <li class="tab" v-for="floor in value.floors">
-                <a class="" v-on:click="displayRooms(floor.id, value.id)">{{floor.name}}</a>
-              </li>
-            </ul>
-          </div>
-          <div :id="value.id" class="card-content grey lighten-4">
-
-          </div>
-          <div class="card-action">
-            <a href="#">Edit</a>
-            <a href="#">Delete</a>
+  <div class="container">
+    <div class="row">
+          <div class="col s12 m6" v-for="value in allData">
+            <div class="card darken-2">
+              <div class="card-content">
+                <span class="card-title">{{value.name}}</span>
+                <ul>
+                  <li>Floors: {{value.floors.length}}</li>
+                  <li>Rooms: {{value.rooms.length}}</li>
+                  <!-- <li>qm: {{value.floors}}</li> -->
+                </ul>
+              </div>
+              <div class="card-tabs">
+                <ul class="tabs tabs-fixed-width">
+                  <li class="tab" v-for="(floor,index) in value.floors">
+                    <a :class="{active: tabActiveIndex===index && tabActiveObjId===value.id}" v-on:click="displayRooms(floor.id, value.id), tooggleActive(index,value.id)">{{floor.name}}</a>
+                  </li>
+                </ul>
+              </div>
+              <div :id="value.id" class="card-content grey lighten-4">
+              </div>
+              <div class="card-action">
+                <a href="#">Edit</a>
+                <a href="#">Delete</a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </li>
-</ul>
+  </div>
 </template>
 
 <script>
@@ -40,7 +37,9 @@ export default {
       allData: [],
       resource: {},
       visibleRooms: ``,
-      roomElement: undefined
+      roomElement: undefined,
+        tabActiveIndex: undefined,
+        tabActiveObjId: undefined
     };
   },
   methods: {
@@ -80,7 +79,11 @@ export default {
       });
 
       document.querySelector(`#${buildingId}`).innerHTML = this.visibleRooms;
-    }
+    },
+      tooggleActive(index,id){
+        this.tabActiveIndex = index;
+        this.tabActiveObjId = id;
+      }
   },
   created() {
     const customActions = {
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
   <nav>
     <div class="container">
       <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
+        <a href="#" class="brand-logo"><i class="material-icons">home</i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <router-link tag="li" to="/" active-class="active" exact><a>Home</a></router-link>
           <router-link tag="li" to="/new" active-class="active"><a>New</a></router-link>
